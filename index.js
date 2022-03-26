f922f9293889e726c41c71b51c81e9c13d8babdb
David Maene <davidmened@gmail.com>
2022-03-26 21:14:05 +0200

    up me line

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 (() => {
     let i = 0;
+    
     const arr = [
         'da',
         'vi',
@@ -9,13 +10,19 @@
         'e'
     ];
 
-    const _handleClick = () => {
-        if(i <= arr.length) i++;
+    const _handleClick = (initialize) => {
+
+        if(i <= arr.length) initialize ? i = 0 :  i++;
         else i = 0;
-
         
+        document.getElementById("postion").innerHTML = i + 1;
+        document.getElementById("value").innerHTML = arr[i];
     }
+    _handleClick(true);
 
-    document.getElementById("btn").onclick = _handleClick
+    document.getElementById("btn").onclick = (e) => {
+        e.preventDefault()
+        _handleClick()
+    }
 
 })()
