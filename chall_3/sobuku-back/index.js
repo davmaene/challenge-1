62188760d2a8b1dfb9aafc039707ac7b18554c90
David Maene <davidmened@gmail.com>
2022-09-30 11:02:19 +0200

    up me line

---
+++ chall_3/sobuku-back/index.js
@@ -0,0 +1,29 @@
+const { levels } = require("./modules/_create.levels");
+
+const onReordone = (
+    { 
+        options: {
+            level,
+            comp
+        } 
+    }
+) => {
+
+    const m = level && level['dimensionsMatrice'];
+    const n = level && level['dimensionsMatrice'];
+    const vercteurs = [];
+
+    for(let i; i < m; i++){
+        for(let k; k < n; k++){
+            
+        }
+    }
+}
+
+onReordone({
+    options: {
+        level: levels['_level_1'],
+        comp: {}
+    }
+})
+
---
+++ chall_3/sobuku-back/modules/_create.levels.js
@@ -0,0 +1,18 @@
+const levels = {
+    _level_1 : {
+        name: "Niveau 1",
+        dimensionsMatrice: 3 
+    },
+    _level_2 : {
+        name: "Niveau 2",
+        dimensionsMatrice: 4
+    },
+    _level_3 : {
+        name: "Niveau 3",
+        dimensionsMatrice: 5 
+    } 
+};
+
+module.exports = {
+    levels
+}
